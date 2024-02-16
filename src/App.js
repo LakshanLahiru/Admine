b3da1fe7edf7ce861b0bd5b0c1dbd66039bd6414
LakshanLahiru <lakshanlahiru200@gmail.com>
2024-02-16 13:47:20 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Navi from './components/Navi/Navi';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importing Routes and Route from react-router-dom
+import Dashboard from './components/Dashboard/Dashboard';
+import AddBooking from './components/Booking/AddBooking';
+import AddDrivers from './components/Master/AddDrivers';
+import Expiry from './components/Notification/Expiry';
+import Heading from './components/Header/Heading';
+import FillDetails from './components/Master/details/FillDetails';
+import FillBooking from './components/Booking/Details/FillBooking';
+
 
 function App() {
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    
+    
+  <Heading />
+   <Navi />
+   <Routes>
+    <Route path="/dashboard" element={<Dashboard />}></Route>
+    <Route path="/addbooking" element={<AddBooking />}></Route>
+    <Route path="/adddrivers" element={<AddDrivers />}></Route>
+    <Route path="/expiry" element={<Expiry />}></Route>
+    <Route path="/adddrivers/details" element={<FillDetails/>}></Route>
+    <Route path="/addbooking/details" element={<FillBooking/>}></Route>
+    
+  </Routes>
+    
+   </Router>
+
   );
 }
 
 export default App;
+
